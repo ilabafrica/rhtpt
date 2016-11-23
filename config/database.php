86d43c7bac6877b7d8a8506ecc0eaa65c04f9b31
kitsao <kittemm@gmail.com>
2016-11-23 14:59:06 +0300

    reverted db changes

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'workbench'),
+            'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', 'xp'),
             'charset'   => 'utf8',
