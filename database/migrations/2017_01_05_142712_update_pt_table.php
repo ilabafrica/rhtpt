a246200de1889bd723cf36df3e08f3609bca1c06
Kitsao Emmanuel <kittemm@gmail.com>
2017-02-16 21:56:44 +0300

    updated migrations

--- database/migrations/2017_01_05_142712_update_pt_table.php
+++ database/migrations/2017_01_05_142712_update_pt_table.php
@@ -13,18 +13,18 @@ class UpdatePtTable extends Migration
     public function up()
     {
       //  Marking of PT submissions
-      Schema::table('users', function ($table)
+      Schema::table('pt', function ($table)
       {
-        $table->tinyInteger('checks')->default(0);
-        $table->tinyInteger('panel_result')->default(0);
-        $table->tinyInteger('incorrect_results')->default(0);
-        $table->tinyInteger('incomplete_kit_data')->default(0);
-        $table->tinyInteger('dev_from_procedure')->default(0);
-        $table->tinyInteger('incomplete_other_information')->default(0);
-        $table->tinyInteger('use_of_expired_kits')->default(0);
-        $table->tinyInteger('invalid_results')->default(0);
-        $table->tinyInteger('wrong_algorithm')->default(0);
-        $table->tinyInteger('incomplete_results')->default(0);
+        $table->tinyInteger('checks')->default(0)->after('panel_status');
+        $table->tinyInteger('panel_result')->default(0)->after('checks');
+        $table->tinyInteger('incorrect_results')->default(0)->after('panel_result');
+        $table->tinyInteger('incomplete_kit_data')->default(0)->after('incorrect_results');
+        $table->tinyInteger('dev_from_procedure')->default(0)->after('incomplete_kit_data');
+        $table->tinyInteger('incomplete_other_information')->default(0)->after('dev_from_procedure');
+        $table->tinyInteger('use_of_expired_kits')->default(0)->after('incomplete_other_information');
+        $table->tinyInteger('invalid_results')->default(0)->after('use_of_expired_kits');
+        $table->tinyInteger('wrong_algorithm')->default(0)->after('invalid_results');
+        $table->tinyInteger('incomplete_results')->default(0)->after('wrong_algorithm');
       });
     }
 
@@ -36,7 +36,7 @@ class UpdatePtTable extends Migration
     public function down()
     {
         //  Drop columns
-        Schema::table('users', function ($table)
+        Schema::table('pt', function ($table)
         {
             $table->dropColumn(['checks', 'panel_result', 'incorrect_results', 'incomplete_kit_data', 'dev_from_procedure', 'incomplete_other_information', 'use_of_expired_kits', 'invalid_results', 'wrong_algorithm', 'incomplete_results']);
         });
