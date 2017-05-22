59c536b82ea6f9b4cd15076231113496a9ce2307
Kitsao Emmanuel <kittemm@gmail.com>
2017-05-22 09:08:12 +0300

    user soft delete

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
-use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use DB;
 
