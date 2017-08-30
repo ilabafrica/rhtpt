e025669a56695c673dc5b4d29e27ca651ac1c718
ken <kennethsila2@gmail.com>
2017-08-30 16:53:32 +0300

    Added information popover on the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,6 +23,7 @@
     <!-- Sweet Alert Styling -->
     <link href="{{ asset('css/sweetalert.css') }}" rel="stylesheet">
     
+    
 </head>
 
 <body>
@@ -58,22 +59,22 @@
                     <div class="form-group row">
                         <label for="username" class="col-md-2 col-form-label">Username</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="username" placeholder="Username" data-toggle="tooltip" data-placement="top" title="Enter Username or PT Tester Enrollment ID which you got at the time of registration">
+                            <input type="text" class="form-control" name="username" placeholder="Username" data-toggle="popover" title="Username/PT Enrolment ID" data-content="Enter Username or PT Tester Enrollment ID which you got at the time of registration" data-placement="top" data-trigger="hover">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="inputPassword3" class="col-md-2 col-form-label">Password</label>
                         <div class="col-md-10">
-                            <input type="password" class="form-control" name="password" placeholder="Password " data-toggle="tooltip" data-placement="top" title="Enter Password that you use to Login">
+                            <input type="password" class="form-control" name="password" placeholder="Password " data-toggle="popover" data-placement="top" data-trigger="hover" title="Password" data-content="Enter Password that you use to Login">
                         </div>
                     </div>
                     <div class="form-group row">
                         <div class="col-md-offset-2 col-md-10">
-                            <button class="btn btn-primary btn-block" type="submit" name="signin" data-toggle="tooltip" data-placement="top" title="Click to Login">LOGIN</button>
-                            <a class="btn btn-wisteria btn-block" onclick="confirmRegistration()" data-toggle="tooltip" data-placement="top" title="Click to Register a new participant">REGISTER HERE</a>
-                            <a class="btn btn-link btn-block" href="{{url('password/reset')}}" data-toggle="tooltip" data-placement="top" title="Click and enter Tester ID to get reset password link in your email">FORGOT PASSWORD</a>
+                            <button class="btn btn-primary btn-block" type="submit" name="signin" data-toggle="popover" data-placement="top" title="Login" data-trigger="hover" data-content="Click to LogIn"  >LOGIN</button>
+                            <a class="btn btn-wisteria btn-block" onclick="confirmRegistration()" data-toggle="popover" data-placement="top" title="Register" data-content="Click to Register a new participant" data-trigger="hover">REGISTER HERE</a>
+                            <a class="btn btn-link btn-block" href="{{url('password/reset')}}" data-toggle="popover" data-placement="top" title="Forgot password" data-trigger="hover" data-content="Click and enter Tester ID to get reset password link in your email">FORGOT PASSWORD</a>
                             <hr>
-                            <h6 class="text-md-center">Designed for <a href="http://www.nphls.or.ke" data-toggle="tooltip" data-placement="top" title="Click to go to NPHL website">NPHL</a> by <a href="//www.ilabafrica.ac.ke" data-toggle="tooltip" data-placement="top" title="Click to go to iLabAfrica website" >@iLabAfrica</a></h6>
+                            <h6 class="text-md-center">Designed for <a href="http://www.nphls.or.ke" data-toggle="popover" data-placement="top" title="NPHL" data-trigger="hover" data-content="Click to go to NPHL website">NPHL</a> by <a href="//www.ilabafrica.ac.ke" data-toggle="popover" data-placement="top" data-trigger="hover" title="iLabAfrica" data-content="Click to go to iLabAfrica website" >@iLabAfrica</a></h6>
                         </div>
                     </div>
                 </form>
@@ -83,12 +84,21 @@
 
 </body>
     <!-- Vue JS -->
-    <script src="{{ asset('js/jquery-1.12.3.min.js') }}"></script>
+    
     <script src="{{ asset('js/vue.min.js') }}"></script>
     <script src="{{ asset('js/vue-resource.min.js') }}"></script>
     <script src="{{ asset('js/vee-validate.js') }}"></script>
     <!-- Sweet Alert -->
     <script src="{{ asset('js/sweetalert.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-1.12.3.min.js') }}"></script>
+    <script src="{{ asset('js/tether.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script>
+        $(function () {
+           $('[data-toggle="popover"]').popover()
+          });
+       
+    </script>
     <script type="text/javascript">
         function confirmRegistration()
         {
