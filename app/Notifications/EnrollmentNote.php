dabbae5cc971c6cb1993dbc732c146a3b5ab27d1
annchemu <annchemu@gmail.com>
2017-09-04 10:52:12 +0300

    deleted unnecessary files (images and excel files)

---
+++ app/Notifications/EnrollmentNote.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class EnrollmentNote extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hello '.$this->user->name)
+            ->line('You have been enrolled into '.$this->user->round)           
+            ->line('NHRL has dispatched your PT Panel for '.$this->user->round'. If not received within 7 days, contact NHRL on 0722934622 or NHPL Help Desk in the link below');
+            ->line('Kindly wait for the shippment of materials to arrive');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
