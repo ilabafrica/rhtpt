d3d5a84ff8ecb096a51bac0653d7519e1ac63008
Piusyo <pmpiusyo@gmail.com>
2017-09-13 17:56:09 +0300

    UI improvements
    password reset page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,55 +1,55 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+<!-- Custom Styling -->
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+<style>
+.form-control:focus {
+    border-color: #006400;
+    outline: none;
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
+   }  
+</style>
+<div class="resetpassword" style="padding-top:20px;">
+
+    <div class="card col-md-3" style="margin:auto; float:none">
+            @if (session('status'))
+                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-                        {{ csrf_field() }}
-
+        <form class="form-signin"  role="form" method="post" action="{{ url('/password/reset') }}">
+            {{ csrf_field() }}
+            <div style="text-align:center;">
+              <img src="{{ '../'.Config::get('cms.logo') }}" style="max-height:120px;margin:0 auto !important;">
+                <h3 class="form-signin-heading">
+                    <span>Kenya Serology Rapid HIV PT</span>
+                </h3>               
+            </div> 
+            <div class="bs-callout bs-callout-info text-left">
+            <h4 class="md-18">Reset Passowrd</h4>
+            <p> Enter your email and your new password. Keep your password safe for your next login.</p>
+            </div>  
                         <input type="hidden" name="token" value="{{ $token }}">
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
+                            <label for="email" >E-Mail Address</label>
                                 <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
+                            <label for="password">Password</label>
                                 <input id="password" type="password" class="form-control" name="password" required>
-
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        </div>
-
+                         </div>
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
+                            <label for="password-confirm" >Confirm Password</label>
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
                                 @if ($errors->has('password_confirmation'))
@@ -57,9 +57,7 @@
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
@@ -69,8 +67,5 @@
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
