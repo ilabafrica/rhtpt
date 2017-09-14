c3f49e021fcebdad3a4ed6b5222251fd13e100d6
kitsao <kittemm@gmail.com>
2017-09-14 11:57:04 +0300

    cool ui

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,23 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<!-- Custom Styling -->
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-<style>
-.form-control:focus {
-    border-color: #006400;
-    outline: none;
-
-   }  
-
-     </style>
-      <div class="resetpassword" style="padding-top:20px;">
-
+<div class="resetpassword" style="padding-top:20px;">
     <div class="card col-md-4" style="margin:auto; float:none">
-            @if (session('status'))
-                         <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        @if (session('status'))
+             <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
         <form class="form-signin"  role="form" method="post" action="{{ url('/password/email') }}">
             {{ csrf_field() }}
             <div style="text-align:center;">
@@ -26,26 +15,22 @@
                     <span>Kenya Serology Rapid HIV PT</span>
                 </h3>               
             </div> 
-
-        <div class="bs-callout bs-callout-info text-left">
-    <h4 class="md-18">Password Recovery</h4>
-    <p> If you have forgotten your password, fill in your PT Enrollment ID below and click Reset Password button. Instructions will be sent to your registered email on how to reset your password.</p>
-  </div>   
-             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <div class="bs-callout bs-callout-info text-left">
+                <h4 class="md-18">Password Recovery</h4>
+                <p> If you have forgotten your password, fill in your PT Enrollment ID below and click Reset Password button. Instructions will be sent to your registered email on how to reset your password.</p>
+            </div>   
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                 <label for="email">PT Enrollment ID</label>
-                 <input id="email" type="email" class="form-control" name="email" placeholder="eg. 19595" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                <input id="email" type="email" class="form-control" name="email" placeholder="eg. 19595" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
             </div>            
-            <button style="background-color: #3498db;border-color: #3498db" type="submit" class="btn btn-md btn-info btn-block">
-           Reset Password
-              </button>
+            <button style="background-color: #3498db;border-color: #3498db" type="submit" class="btn btn-md btn-info btn-block">Reset Password</button>
             <br><a href="/login" style="color:#18bc9c">No, I remember my password.</a>
         </form>
-            </div> 
-    </div>
+      </div> 
+</div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,71 +1,64 @@
 @extends('layouts.app')
 @section('content')
-<!-- Custom Styling -->
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-<style>
-.form-control:focus {
-    border-color: #006400;
-    outline: none;
-
-   }  
-</style>
 <div class="resetpassword" style="padding-top:20px;">
-
     <div class="card col-md-3" style="margin:auto; float:none">
-            @if (session('status'))
-                         <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-        <form class="form-signin"  role="form" method="post" action="{{ url('/password/reset') }}">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <form class="form-signin" role="form" method="post" action="{{ url('/password/reset') }}">
             {{ csrf_field() }}
             <div style="text-align:center;">
-              <img src="{{ '../'.Config::get('cms.logo') }}" style="max-height:120px;margin:0 auto !important;">
+                <img src="{{ '../../'.Config::get('cms.logo') }}" height="75px;">
                 <h3 class="form-signin-heading">
                     <span>Kenya Serology Rapid HIV PT</span>
                 </h3>               
             </div> 
             <div class="bs-callout bs-callout-info text-left">
-            <h4 class="md-18">Reset Passowrd</h4>
-            <p> Enter your email and your new password. Keep your password safe for your next login.</p>
+                <h4 class="md-18">Reset Passowrd</h4>
+                <p> Enter your email and your new password. Keep your password safe for your next login.</p>
             </div>  
-                        <input type="hidden" name="token" value="{{ $token }}">
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" >E-Mail Address</label>
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password">Password</label>
-                                <input id="password" type="password" class="form-control" name="password" required>
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                         </div>
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" >Confirm Password</label>
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+            <input type="hidden" name="token" value="{{ $token }}">
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" >E-Mail Address</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password">Password</label>
+                <input id="password" type="password" class="form-control" name="password" required>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+             </div>
+            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                <label for="password-confirm" >Confirm Password</label>
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reset Password
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                @if ($errors->has('password_confirmation'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group" style="padding-bottom:30px;">
+                <div class="col-md-7">
+                    <a href="/login" style="color:#18bc9c">No, I remember my password.</a>
                 </div>
+                <div class="col-md-5">
+                    <button type="submit" class="btn btn-success pull-right">
+                        Reset Password
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
