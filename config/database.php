87f456a836487c505b599360466307c279cc700a
bmaiyo <bmaiyo@strathmore.edu>
2018-01-16 09:34:29 +0300

    updates to db config

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'PT'),
+            'database' => env('DB_DATABASE', 'clean'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
